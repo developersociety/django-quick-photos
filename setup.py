e8327422d3a394ccb2a1914fb57adc5322993659
Alex Tomkins <tomkins@darkzone.net>
2015-07-09 15:26:57 +0100

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 # Use quickphotos.VERSION for version numbers
 version_tuple = __import__('quickphotos').VERSION
@@ -17,12 +17,7 @@ setup(
     install_requires=[
         'python-instagram>=0.8.0',
     ],
-    packages=[
-        'quickphotos',
-        'quickphotos.management',
-        'quickphotos.management.commands',
-        'quickphotos.templatetags',
-    ],
+    packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
