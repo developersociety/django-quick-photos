db7df35458ac132bb84355df1cf2a5e329ca1d84
Alex Tomkins <tomkins@darkzone.net>
2016-01-08 11:58:31 +0000

    Add support for multiple users photos

--- quickphotos/templatetags/quickphotos_tags.py
+++ quickphotos/templatetags/quickphotos_tags.py
@@ -6,8 +6,12 @@ register = template.Library()
 
 
 @register.assignment_tag
-def get_latest_photos(user, limit=None):
-    photos = Photo.objects.filter(user=user)
+def get_latest_photos(*args, **kwargs):
+    limit = kwargs.pop('limit', None)
+    photos = Photo.objects.all()
+
+    if args:
+        photos = photos.filter(user__in=args)
 
     if limit is not None:
         photos = photos[:limit]
