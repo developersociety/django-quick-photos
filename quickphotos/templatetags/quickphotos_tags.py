980344d2fdf6bad22fa3bbcd7153db28603a14be
Alex Tomkins <tomkins@darkzone.net>
2016-01-08 12:09:14 +0000

    Tag filtering by tag

--- quickphotos/templatetags/quickphotos_tags.py
+++ quickphotos/templatetags/quickphotos_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from quickphotos.models import Photo
+from quickphotos.models import Photo, Tag
 
 register = template.Library()
 
@@ -9,10 +9,18 @@ register = template.Library()
 def get_latest_photos(*args, **kwargs):
     limit = kwargs.pop('limit', None)
     liked_by = kwargs.pop('liked_by', None)
+    tag = kwargs.pop('tag', None)
     photos = Photo.objects.all()
 
     if liked_by:
         photos = photos.filter(like__user=liked_by)
+
+    if tag:
+        try:
+            tag_obj = Tag.objects.get(name=tag)
+            photos = photos.filter(tags=tag_obj)
+        except Tag.DoesNotExist:
+            photos = photos.none()
 
     if args:
         photos = photos.filter(user__in=args)
