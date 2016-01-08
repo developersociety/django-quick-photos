4af6e0b4c514b65c5bd9251398947e830ce9f26e
Alex Tomkins <tomkins@darkzone.net>
2016-01-08 12:03:33 +0000

    Tag filtering by liked photos

--- quickphotos/templatetags/quickphotos_tags.py
+++ quickphotos/templatetags/quickphotos_tags.py
@@ -8,7 +8,11 @@ register = template.Library()
 @register.assignment_tag
 def get_latest_photos(*args, **kwargs):
     limit = kwargs.pop('limit', None)
+    liked_by = kwargs.pop('liked_by', None)
     photos = Photo.objects.all()
+
+    if liked_by:
+        photos = photos.filter(like__user=liked_by)
 
     if args:
         photos = photos.filter(user__in=args)
